4dec4b565178874c41004cf6747698670b21559b
Keane <KeaneCarotenuto@gmail.com>
2021-02-14 13:13:44 +1300

    Basic Ground Texture Gen

---
+++ Assets/GenerateTilemap.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GenerateTilemap : MonoBehaviour
+{
+
+    public Tilemap ground;
+    public Tilemap obstacles;
+    public List<Tile> groundTiles;
+    public List<Tile> obstTiles;
+    public int width;
+    public int height;
+
+    [Range(0.0f, 1.0f)] public float groundEmptyChance;
+    [Range(0.0f, 1.0f)] public float obstacleChance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int y = -(height / 2); y < (height/2); y++)
+        {
+            for (int x = -(width / 2); x < (width / 2); x++)
+            {
+                Tile randTile = ((int)Random.Range(0, 1/groundEmptyChance) != 0 ? groundTiles[9] : groundTiles[Random.Range(0, groundTiles.Count)]);
+
+                ground.SetTile(new Vector3Int(x, y, 0), randTile);
+            }
+        }
+
+        for (int y = -(height / 2); y < (height / 2); y++)
+        {
+            for (int x = -(width / 2); x < (width / 2); x++)
+            {
+                Tile randTile = ((int)Random.Range(0, 1/obstacleChance) != 0 ? null : obstTiles[Random.Range(0, obstTiles.Count)]);
+
+                if (randTile != null)
+                {
+                    obstacles.SetTile(new Vector3Int(x, y, 0), randTile);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
